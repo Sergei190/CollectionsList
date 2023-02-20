a9d7959c6481559c0278737dd4c230ee0ab7e8b1
Sergei190 <kudrasovcergej@gmail.com>
2023-02-20 22:39:37 +0500

    Errors have been fixed

--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,22 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        String purchase;
+    public static String purchase;
 
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         ArrayList<String> list = new ArrayList<>();
 
         while (true) {
-            getPrintln("Выберите операцию:");
-            getPrintln("1. Операция добавить");
-            getPrintln("2. Операция показать");
-            getPrintln("3. Операция удалить");
-            getPrintln("4. Введите текст для поиска:");
-
-            purchase= scanner.nextLine();
+            mainMenu(scanner);
 
             if (purchase.equalsIgnoreCase("end")) {
                 break;
             } else {
                 switch (purchase) {
                     case ("1"): {
-                        getPrintln("Какую покупку хотите добавить?");
-                        purchase = scanner.nextLine();
-                        list.add(purchase);
-                        getPrintln("Итого в списке покупок: " + list.size());
+                        itemToBuy(scanner, list);
                         break;
                     }
                     case ("2"): {
@@ -34,53 +25,74 @@ public class Main {
                         break;
                     }
                     case ("3"): {
-                        getPrintln("Какую хотите удалить? Введите номер или название");
-                        show(list);
-                        purchase = scanner.nextLine();
-                        try {
-                            list.remove(Integer.parseInt(purchase) - 1);
-                        } catch (NumberFormatException e) {
-                            for (int i = 0; i < list.size(); i++) {
-                                if (list.get(i).equalsIgnoreCase(purchase)) {
-                                    list.remove(i);
-                                    getPrintln("Пункт покупки удален!");
-                                } else {
-                                    getPrintln("Такой элемент не найден!");
-                                }
-                            }
-                        }
+                        deleteItem(scanner, list);
                         show(list);
                         break;
                     }
                     case ("4"): {
-                        getPrintln("Введите текст для поиска:");
-                        purchase = scanner.nextLine();
-                        try {
-                            getPrintln("Найдено: " + purchase + ". " + list.get(Integer.parseInt(purchase) - 1));
-                        } catch (NumberFormatException e) {
-                            String lowerCase;
-                            String scanLow = purchase.toLowerCase();
-                            for (int i = 0; i < list.size(); i++) {
-                                lowerCase = list.get(i).toLowerCase();
-                                if (lowerCase.contains(scanLow)) {
-                                    getPrintln((list.indexOf(list.get(i)) + 1) + ". " + list.get(i));
-                                }
-                            }
-                        }
+                        findItem(scanner, list);
                         break;
                     }
+
                 }
             }
         }
     }
 
-    private static void getPrintln(String x) {
-        System.out.println(x);
+    private static void mainMenu(Scanner scanner) {
+        System.out.println("Выберите операцию или ведите end для завершения:");
+        System.out.println("1. Операция добавить");
+        System.out.println("2. Операция показать");
+        System.out.println("3. Операция удалить");
+        System.out.println("4. Операция найти");
+
+        purchase = scanner.nextLine();
+    }
+
+    private static void deleteItem(Scanner scanner, ArrayList<String> list) {
+        System.out.println("Какую хотите удалить? Введите название покупки");
+        show(list);
+        purchase = scanner.nextLine();
+        try {
+            list.remove(Integer.parseInt(purchase) - 1);
+        } catch (NumberFormatException e) {
+            for (int i = 0; i < list.size(); i++) {
+                if (list.get(i).equalsIgnoreCase(purchase)) {
+                    list.remove(i);
+                    System.out.println("Покупка удалена, список покупок:");
+                }
+            }
+        }
+    }
+
+
+    private static void itemToBuy(Scanner scanner, ArrayList<String> list) {
+        System.out.println("Какую покупку хотите добавить?");
+        purchase = scanner.nextLine();
+        list.add(purchase);
+        System.out.println("Итого в списке покупок: " + list.size());
+    }
+
+    private static void findItem(Scanner scanner, ArrayList<String> list) {
+        System.out.println("Введите текст для поиска: ");
+        purchase = scanner.nextLine();
+        try {
+            System.out.println("Найдено: " + purchase + ". " + list.get(Integer.parseInt(purchase) - 1));
+        } catch (NumberFormatException e) {
+            String lowerCase;
+            String scanLow = purchase.toLowerCase();
+            for (int i = 0; i < list.size(); i++) {
+                lowerCase = list.get(i).toLowerCase();
+                if (lowerCase.contains(scanLow)) {
+                    System.out.println((list.indexOf(list.get(i)) + 1) + ". " + list.get(i));
+                }
+            }
+        }
     }
 
     public static void show(ArrayList<String> list) {
         for (String i : list) {
-            getPrintln(list.indexOf(i) + 1 + ". " + i);
+            System.out.println(list.indexOf(i) + 1 + ". " + i);
         }
     }
 }
